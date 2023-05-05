a4550239c9ea1328b6741b0b6593af991ce18aa7
zsunreal <173136977@qq.com>
2023-05-05 10:41:38 +0800

    合并代码

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -458,13 +458,8 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
         show_progress=True,
     )
 
-logging.info(
-    colorama.Back.GREEN
-    + "\n川虎的温馨提示：访问 http://localhost:7860 查看界面"
-    + colorama.Style.RESET_ALL
-)
 # 默认开启本地服务器，默认可以直接从IP访问，默认不创建公开分享链接
-demo.title = i18n("川虎Chat 🚀")
+demo.title = i18n("小亮Chat 👀")
 
 if __name__ == "__main__":
     reload_javascript()
